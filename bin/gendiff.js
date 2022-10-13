016257b2992bb8729fb1711e47f8999907f751b3
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-10-13 22:24:47 +0300

    connected function module

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
-#!/bin/env node
+#!/usr/bin/env node
 
 import { Command } from 'commander';
+import gendiff from '../index.js';
 
 const program = new Command();
 
@@ -8,8 +9,9 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
   .argument('<filepath1>')
   .argument('<filepath2')
-  .option('-f, --format <type>', 'output format');
+  .action((filepath1, filepath2) => gendiff(filepath1, filepath2));
 
 program.parse();
