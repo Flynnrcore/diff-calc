90897f03baa50158fad03be4fb5a81ac053e26ef
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-10-15 00:48:14 +0300

    add compare function

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 const getPath = (file) => path.resolve(process.cwd(), file);
 
-const gendiff = (file1, file2) => {
-  const firstFile = JSON.parse(fs.readFileSync(getPath(file1), 'utf-8'));
-  const secondFile = JSON.parse(fs.readFileSync(getPath(file2), 'utf-8'));
-  console.log(firstFile);
-  console.log(secondFile);
+const getCompareObj = (obj1, obj2) => {
+  const keysObj1 = Object.keys(obj1);
+  const keysObj2 = Object.keys(obj2);
+  const commonKeys = _.sortBy(_.union(keysObj1, keysObj2));
+  const result = commonKeys.reduce((acc, key) => {
+    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+      acc.push(obj2[key] === obj1[key] ? `  ${key}: ${obj2[key]}` : `- ${key}: ${obj1[key]}
++ ${key}: ${obj2[key]}`);
+    } else if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+      acc.push(`- ${key}: ${obj1[key]}`);
+    } else {
+      acc.push(`+ ${key}: ${obj2[key]}`);
+    }
+    return acc;
+  }, []);
+  return result;
 };
 
-export default gendiff;
+const genDiff = (file1, file2) => {
+  const firstFile = JSON.parse(fs.readFileSync(getPath(file1), 'utf-8'));
+  const secondFile = JSON.parse(fs.readFileSync(getPath(file2), 'utf-8'));
+  const result = getCompareObj(firstFile, secondFile);
+  console.log(`{
+${result.join('\n')}
+  }`);
+};
+
+export default genDiff;
