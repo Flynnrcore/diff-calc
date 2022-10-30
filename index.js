a9def9f79ee59869022b11cfdf848edac77e7715
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-10-31 00:34:43 +0300

    add format function import

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import getCompareObj from './src/compare.js';
 import getParse from './src/parsers.js';
+import stylish from './src/stylish.js';
 
 const genDiff = (file1, file2) => {
   const firstFile = getParse(file1);
   const secondFile = getParse(file2);
   const compareFiles = getCompareObj(firstFile, secondFile);
-  const result = `{
-${compareFiles.join('\n')}
-}`;
+  const result = stylish(compareFiles);
 
   console.log(result);
   return result;
