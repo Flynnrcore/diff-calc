10d803dc7d1d9df73da2e2d1f1f80dc6b2bf17c0
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-11-04 09:22:49 +0300

    search errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,59 @@
 import { test, expect } from '@jest/globals';
+//import { fileURLToPath } from 'url';
+//import getParse from '../src/parsers.js';
+//import fs from 'fs';
+//import path from 'path';
 import genDiff from '../index.js';
 
+/* const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8'); */
+
+const resultJSON = `{
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow: 
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+}`;
 const expectResult = `{
   - follow: false
     host: hexlet.io
@@ -12,7 +65,12 @@ const expectResult = `{
 
 test('getCompareJSON', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectResult);
+  expect(genDiff('__fixtures__/filepath1.json','__fixtures__/filepath2.json')).toEqual(resultJSON);
 });
+
+/* test('getCompareJSON', () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectResult);
+}); */
 
 test('getCompareYML', () => {
   expect(genDiff('__fixtures__/filepath1.yml', '__fixtures__/filepath2.yml')).toEqual(expectResult);
--- src/compare.js
+++ src/compare.js
@@ -1,25 +1,43 @@
 import _ from 'lodash';
 
+const stringify = (value, replacer = ' ', spacesCount = 1) => {
+  const iter = (currentValue, depth) => {
+    if (typeof (currentValue) !== 'object' || currentValue === null) {
+      return String(currentValue);
+    }
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+
+    const arrValue = Object.entries(currentValue);
+    const lines = arrValue.map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+    const result = ['{', ...lines, `${bracketIndent}}`].join('\n');
+
+    return result;
+  };
+
+  return iter(value, 1);
+};
+
 const getCompareObj = (obj1, obj2) => {
   const commonKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
   const result = commonKeys.reduce((acc, key) => {
     if (typeof (obj1[key]) === 'object' && typeof (obj2[key]) === 'object') {
       acc.push({ type: 'nested', key: `${key}`, children: getCompareObj(obj1[key], obj2[key]) });
     } else if (!Object.hasOwn(obj1, key)) {
-      acc.push({ type: 'added', key: `${key}`, value: `${obj2[key]}` });
+      acc.push({ type: 'added', key: `${key}`, value: `${stringify(obj2[key])}` });
     } else if (!Object.hasOwn(obj2, key)) {
-      acc.push({ type: 'deleted', key: `${key}`, value: `${obj1[key]}` });
+      acc.push({ type: 'deleted', key: `${key}`, value: `${stringify(obj1[key])}` });
     } else if (obj1[key] !== obj2[key]) {
       acc.push({
-        type: 'changed', key: `${key}`, value1: `${obj1[key]}`, value2: `${obj2[key]}`,
+        type: 'changed', key: `${key}`, value1: `${stringify(obj1[key])}`, value2: `${stringify(obj2[key])}`,
       });
     } else if (obj1[key] === obj2[key]) {
-      acc.push({ type: 'unchanged', key: `${key}`, value: `${obj1[key]}` });
+      acc.push({ type: 'unchanged', key: `${key}`, value: `${stringify(obj1[key])}` });
     }
 
     return acc;
   }, []);
-
   return result;
 };
 
