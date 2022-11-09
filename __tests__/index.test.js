8e8b6c553febdc9c97a84e77cd043ae9d21c93a7
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-11-09 21:40:55 +0300

    test errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,59 +1,16 @@
 import { test, expect } from '@jest/globals';
-//import { fileURLToPath } from 'url';
-//import getParse from '../src/parsers.js';
-//import fs from 'fs';
-//import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
 import genDiff from '../index.js';
 
-/* const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8'); */
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const resultJSON = `{
-  common: {
-    + follow: false
-      setting1: Value 1
-    - setting2: 200
-    - setting3: true
-    + setting3: null
-    + setting4: blah blah
-    + setting5: {
-          key5: value5
-      }
-      setting6: {
-          doge: {
-            - wow: 
-            + wow: so much
-          }
-          key: value
-        + ops: vops
-      }
-  }
-  group1: {
-    - baz: bas
-    + baz: bars
-      foo: bar
-    - nest: {
-          key: value
-      }
-    + nest: str
-  }
-- group2: {
-      abc: 12345
-      deep: {
-          id: 45
-      }
-  }
-+ group3: {
-      deep: {
-          id: {
-              number: 45
-          }
-      }
-      fee: 100500
-  }
-}`;
+const resultJSON = readFile('result.yml');
+
 const expectResult = `{
   - follow: false
     host: hexlet.io
@@ -64,14 +21,9 @@ const expectResult = `{
 }`;
 
 test('getCompareJSON', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectResult);
-  expect(genDiff('__fixtures__/filepath1.json','__fixtures__/filepath2.json')).toEqual(resultJSON);
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(resultJSON);
 });
 
-/* test('getCompareJSON', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectResult);
-}); */
-
-test('getCompareYML', () => {
+/* test('getCompareYML', () => {
   expect(genDiff('__fixtures__/filepath1.yml', '__fixtures__/filepath2.yml')).toEqual(expectResult);
-});
+}); */
--- src/stylish.js
+++ src/stylish.js
@@ -1,24 +1,47 @@
 const stylish = (arr, replacer = ' ', spaceCount = 2) => {
+  const stringify = (value, replace = ' ', spacesCount = 1) => {
+    const iter = (currentValue, depth) => {
+      if (typeof (currentValue) !== 'object' || currentValue === null) {
+        return String(currentValue);
+      }
+      const indentSize = depth * spacesCount;
+      const currentIndent = replace.repeat(indentSize);
+      const bracketIndent = replace.repeat(indentSize - spacesCount);
+
+      const arrValue = Object.entries(currentValue);
+      const lines = arrValue.map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+      const result = ['{', ...lines, `${bracketIndent}}`].join('\n');
+
+      return result;
+    };
+
+    return iter(value, 1);
+  };
+
   const iter = (currentValue, depth) => {
+    if (typeof (currentValue) !== 'object' || currentValue === null) {
+      return String(currentValue);
+    }
+
     const indentSize = depth * spaceCount;
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spaceCount);
 
     const lines = currentValue.map((obj) => {
       if (obj.type === 'nested') {
-        return `${currentIndent}  ${obj.key}: ${iter(obj.children, depth + 2)}`;
+        return `${currentIndent}${obj.key}: ${iter(obj.children, depth + 1)}`;
       }
 
       switch (obj.type) {
         case 'changed':
-          return `${currentIndent}- ${obj.key}: ${obj.value1}
-${currentIndent}+ ${obj.key}: ${obj.value2}`;
+          return `${currentIndent}- ${obj.key}: ${stringify(obj.value1)}
+${currentIndent}+ ${obj.key}: ${stringify(obj.value2)}`;
         case 'deleted':
-          return `${currentIndent}- ${obj.key}: ${obj.value}`;
+          return `${currentIndent}- ${obj.key}: ${stringify(obj.value)}`;
         case 'added':
-          return `${currentIndent}+ ${obj.key}: ${obj.value}`;
+          return `${currentIndent}+ ${obj.key}: ${stringify(obj.value)}`;
         default:
-          return `${currentIndent}  ${obj.key}: ${obj.value}`;
+          return `${currentIndent}  ${obj.key}: ${stringify(obj.value)}`;
       }
     });
 
@@ -29,7 +52,7 @@ ${currentIndent}+ ${obj.key}: ${obj.value2}`;
     ].join('\n');
   };
 
-  return iter(arr, 1);
+  return iter(arr, 2);
 };
 
 export default stylish;
