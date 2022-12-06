ac419ef34872ec9955f92c03401cee4d2b9eb642
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-12-06 23:18:06 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,13 +3,15 @@ import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../index.js';
+import stylish from '../src/formatters/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const resultJSON = readFile('result.yml');
+const resultStylish = readFile('resultStylish.txt');
+const resultPlain = readFile('resultPlain.txt');
 
 const expectResult = `{
   - follow: false
@@ -20,10 +22,11 @@ const expectResult = `{
   + verbose: true
 }`;
 
-test('getCompareJSON', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(resultJSON);
+test('getCompareStylisFormat', () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', stylish)).toEqual(resultStylish);
+  expect(genDiff('__fixtures__/filepath1.yml', '__fixtures__/filepath2.yml')).toEqual(expectResult);
 });
 
-test('getCompareYML', () => {
-  expect(genDiff('__fixtures__/filepath1.yml', '__fixtures__/filepath2.yml')).toEqual(expectResult);
+test('getComparePlainFormat', () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toEqual(resultPlain);
 });
