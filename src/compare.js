9d46bd88e318d8ce95043676c5bdc4b3826f3a1f
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-12-09 01:28:50 +0300

    fix mentors remarks

--- src/compare.js
+++ src/compare.js
@@ -5,7 +5,7 @@ const getCompareObj = (obj1, obj2) => {
 
   const getCompare = (data1, data2, key) => {
     if (typeof (data1[key]) === 'object' && typeof (data2[key]) === 'object') {
-      return { type: 'nested', key: `${key}`, children: getCompareObj(data1[key], data2[key]) };  
+      return { type: 'nested', key: `${key}`, children: getCompareObj(data1[key], data2[key]) }; 
     }
 
     if (!Object.hasOwn(data1, key)) {
@@ -13,7 +13,7 @@ const getCompareObj = (obj1, obj2) => {
     }
 
     if (!Object.hasOwn(data2, key)) {
-      return { type: 'deleted', key: `${key}`, value: data1[key] }
+      return { type: 'deleted', key: `${key}`, value: data1[key] };
     }
 
     if (data1[key] !== data2[key]) {
